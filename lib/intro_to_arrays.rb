7025b0e14ea368daf084e7e7e9ecc27849094370
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 20:27:13 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1, 2]
+end
+
+def first_element(a)
+  a.first 
+end
+
+def third_element(a)
+  a[2]
+end 
+
+def last_element(a)
+  a.last 
+end 
+
+def first_element_with_array_methods(a)
+  a.first 
+end 
+
+def last_element_with_array_methods(a)
+  a.last 
+end 
+
+def length_of_array(a)
+  a.size 
+end 
